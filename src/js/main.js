d2b1da820404b7507558745fdc1afd2ca6de67e7
Dhanushmathan <lovelymathan2394@gmail.com>
2025-01-23 16:50:03 +0530

    add: futures

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+const signBtnEl = document.getElementById("signBtn");
+const signToggleEL = document.getElementById("signToggle");
+
+signBtnEl.addEventListener("click", () => {
+    // if (signToggleEL.style.display === "none") {
+    //     signToggleEL.style.display = "block";
+    // } else {
+    //     signToggleEL.style.display = "none";
+    // }
+})
